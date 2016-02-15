1f3c48dc4bedf24a465c72a72bd8e44179a52ab5
Afton Downey and Brianna Popson <aaike001@gmail.com>
2016-02-15 09:42:05 -0800

    create first passing spec for Ping

---
+++ src/Pingpong.php
@@ -0,0 +1,12 @@
+<?php
+
+    class PingPong
+    {
+        function makePingPong($input_number)
+        {
+            if (($input_number % 3) == 0)
+                return "Ping";
+        }
+    }
+
+?>
