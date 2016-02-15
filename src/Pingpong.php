04329ee2ad0e919fce05f30192a0448b40d5b57e
Afton Downey and Brianna Popson <aaike001@gmail.com>
2016-02-15 10:22:53 -0800

    add PingPong and final else specs

--- src/Pingpong.php
+++ src/Pingpong.php
@@ -4,11 +4,14 @@
     {
         function makePingPong($input_number)
         {
-            if (($input_number % 3) == 0) {
+            if (($input_number % 15) == 0) {
+                return "Ping Pong";
+            } elseif (($input_number % 3) == 0) {
                 return "Ping";
             } elseif (($input_number % 5) == 0) {
                 return "Pong";
-            }
+            } else
+            return $input_number;
 
         }
     }
--- tests/PingPongTest.php
+++ tests/PingPongTest.php
@@ -30,6 +30,32 @@
             //Assert
             $this->assertEquals("Pong", $result);
         }
+
+        function test_makePingPong_pingpong()
+        {
+            //Arrange
+            $test_PingPong = new PingPong;
+            $input = 15;
+
+            //Act
+            $result = $test_PingPong->makePingPong($input);
+
+            //Assert
+            $this->assertEquals("Ping Pong", $result);
+        }
+
+        function test_makePingPong_number()
+        {
+            //Arrange
+            $test_PingPong = new PingPong;
+            $input = 17;
+
+            //Act
+            $result = $test_PingPong->makePingPong($input);
+
+            //Assert
+            $this->assertEquals(17, $result);
+        }
     }
 
 ?>
